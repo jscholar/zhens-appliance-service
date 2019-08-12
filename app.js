571931b9866a3499be841eaf17b0b56224e4a1e1
jscholar <jinruncai@gmail.com>
2019-08-12 12:08:04 -0700

    Bootstrapabout page

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 
+app.get("/about", (req, res) => {
+    res.render('pages/about/about');
+})
+
 app.get("/", (req, res) => {
     res.render('pages/index/index');
 });
