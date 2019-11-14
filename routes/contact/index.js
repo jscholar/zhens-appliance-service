95f53ebd0cd81d9670a3df69505baa50ad3340a2
jscholar <jinruncai@gmail.com>
2019-11-13 16:12:10 -0800

    Moved all css and scripts to /public.

--- routes/contact/index.js
+++ routes/contact/index.js
@@ -4,8 +4,4 @@ contact.get("/", (req, res) => {
     res.render("pages/contact/contact");
 });
 
-contact.get("/style", (req, res) => {
-    res.sendFile(__dirname + "/styles/contact.css");
-});
-
 module.exports = contact;
