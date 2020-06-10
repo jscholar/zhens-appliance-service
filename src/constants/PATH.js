00e578bbe5e0a30dca7b04e8184dc2b1096cb71f
Jin Cai <jinruncai@gmail.com>
2020-06-10 15:46:10 -0700

    Change publicPath variable name

--- src/constants/PATH.js
+++ src/constants/PATH.js
@@ -1,12 +1,12 @@
 const path = require('path');
 
 export const root = path.resolve(__dirname, '..', '..');
-export const _public = path.resolve(root, 'public');
+export const publicPath = path.resolve(root, 'public');
 export const templates = path.resolve(root, 'src', 'templates');
 
 const PATH = {
   root,
-  public: _public,
+  public: publicPath,
   templates,
 };
 
