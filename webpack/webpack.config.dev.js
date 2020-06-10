0d6636f54bb946294c1c3600354097179672b566
Jin Cai <jinruncai@gmail.com>
2020-06-10 15:29:08 -0700

    Set up webpack-dev-server

---
+++ webpack/webpack.config.dev.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PUBLIC = path.resolve(__dirname, '..', 'public')
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, '..', 'dist', 'client', 'client.js'),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', 'src', 'templates', 'index.html')
+    })
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(PUBLIC, 'scripts'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(PUBLIC),
+    port: 9000,
+  }
+}
