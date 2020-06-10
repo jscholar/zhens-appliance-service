0974c70bc6bdc7d4f4a28ef97a8dc2ed9966baaf
Jin Cai <jinruncai@gmail.com>
2020-06-10 15:45:01 -0700

    Lint webpack config

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -9,7 +9,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', 'src', 'templates', 'index.html'),
-    })
+    }),
   ],
   output: {
     filename: 'bundle.js',
