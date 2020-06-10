66b8a52c1dbe8343b1ede08ebb6866fd260b52b4
Jin Cai <jinruncai@gmail.com>
2020-06-10 12:45:35 -0700

    Move App to client folder and fix argument to ssr

--- src/server/lib/ssr.js
+++ src/server/lib/ssr.js
@@ -1,8 +1,9 @@
+import React from 'react';
 import ReactDOMServer from 'react-dom/server.js';
-import App from '../../App';
+import App from '../../client/App';
 
 const generateDocument = () => {
-  ReactDOMServer.renderToString(App);
+  return ReactDOMServer.renderToString(<App></App>);
 }
 
 export default generateDocument;
