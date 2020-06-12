2c4def1db9c1e6e3d8b522841620413554661fa7
Jin Cai <jinruncai@gmail.com>
2020-06-12 15:15:43 -0700

    Use es6 partial import

--- src/server/lib/template.js
+++ src/server/lib/template.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
 import path from 'path';
-import PATH from '../../constants/PATH';
+import { templates } from '../../constants/PATH';
 
-const document = fs.readFileSync(path.resolve(PATH.templates, 'index.html')).toString();
+const document = fs.readFileSync(path.resolve(templates, 'index.html')).toString();
 
-const template = (app) => {
-  return document.replace('<!-- App -->', app);
-}
+const template = (app) => (
+  document.replace('<!-- App -->', app)
+);
 
 export default template;
