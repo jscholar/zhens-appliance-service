126e91a849b4704405f1c84cd97c113f5b18a3d5
Jin Cai <jinruncai@gmail.com>
2020-06-19 18:02:28 -0700

    Change webpack-dev-server to build from source
    Enables hot reloading without a concurrent and separate
    babel transpilation

--- webpack/webpack.config.dev.js
+++ webpack/webpack.config.dev.js
@@ -5,7 +5,7 @@ const PUBLIC = path.resolve(__dirname, '..', 'public');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, '..', 'dist', 'client', 'index.bundle.js'),
+  entry: path.resolve(__dirname, '..', 'src', 'client', 'index.bundle.jsx'),
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', 'src', 'templates', 'index.html'),
@@ -19,5 +19,19 @@ module.exports = {
   devServer: {
     contentBase: path.resolve(PUBLIC),
     port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [
+          /node_modules/,
+        ],
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
   },
 };
