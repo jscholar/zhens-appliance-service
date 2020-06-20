40007b2dfa670cf4ed211100fc01d9c039554b2b
Jin Cai <jinruncai@gmail.com>
2020-06-19 17:46:44 -0700

    Position page to leave room for questionnaire

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Navbar from './components/Navbar';
 import Landing from './pages/Landing';
 
-const App = () => (
-  <main>
-    <Navbar />
-    <Landing />
-  </main>
-);
+const App = () => {
+  const [active, setActive] = useState(false);
+  const [page, setPage] = useState('landing');
+
+  const qClass = active ? 'questionnaire-active' : 'questionnaire-inactive';
+
+  return (
+    <main>
+      <div className="layout">
+        <Navbar />
+        <div className="pages">
+          <Landing />
+        </div>
+        <div className={`questionnaire ${qClass}`}>
+          Questionnaire
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default App;
