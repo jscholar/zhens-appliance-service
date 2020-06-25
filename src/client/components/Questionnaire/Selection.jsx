0aef2775977792a21282e47a6c6699c33c96284a
Jin Cai <jinruncai@gmail.com>
2020-06-25 16:05:00 -0700

    Add functionality to Selection component

--- src/client/components/Questionnaire/Selection.jsx
+++ src/client/components/Questionnaire/Selection.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Selection = ({ label, svgLink, active, onClick }) => (
-  <div className={`selection ${active ? 'active' : ''}`} onClick={onClick}>
+const Selection = ({
+  label,
+  svgLink,
+  active,
+  handleSelect,
+  index,
+}) => (
+  <div
+    role="grid cell"
+    tabIndex={index}
+    className={`selection ${active ? 'active' : ''}`}
+    onKeyPress={({ key }) => {
+      if (key === 'Enter') handleSelect();
+    }}
+    onClick={handleSelect}
+  >
     <img className="selection-icon" alt={label} src={svgLink} />
     <p>{label}</p>
   </div>
 );
 
 Selection.propTypes = {
+  handleSelect: PropTypes.func,
+  index: PropTypes.number.isRequired,
   label: PropTypes.string.isRequired,
   svgLink: PropTypes.string.isRequired,
   active: PropTypes.bool,
 };
 
 Selection.defaultProps = {
+  handleSelect: () => {},
   active: false,
 };
 
