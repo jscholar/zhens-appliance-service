6075dd98d877dc6a710d0e33dbd592af6b2a3280
Jin Cai <jinruncai@gmail.com>
2020-06-25 15:12:26 -0700

    Add prop types to appliance and remove name

--- src/client/components/Questionnaire/Appliance.jsx
+++ src/client/components/Questionnaire/Appliance.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Appliance = ({ name, label, svgLink }) => (
+const Appliance = ({ label, svgLink }) => (
   <div>
     <img className="appliance-icon" alt={label} src={svgLink} />
     <p>{label}</p>
   </div>
 );
+
+Appliance.propTypes = {
+  label: PropTypes.string.isRequired,
+  svgLink: PropTypes.string.isRequired,
+};
 
 export default Appliance;
