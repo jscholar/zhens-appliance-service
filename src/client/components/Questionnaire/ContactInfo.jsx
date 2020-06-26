ae733f805553f3d894b50d511ccadfdc46fb831d
Jin Cai <jinruncai@gmail.com>
2020-06-25 19:44:11 -0700

    Add inputs in contact info

--- src/client/components/Questionnaire/ContactInfo.jsx
+++ src/client/components/Questionnaire/ContactInfo.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PhoneInput from 'react-phone-input-auto-format';
 
-const ContactInfo = (props) => (
-  <div>
-    Contact Info
-  </div>
-);
+const ContactInfo = ({ answer }) => {
+  const [info, setInfo] = useState({});
+
+  return (
+    <div className="contact">
+      <div className="prompt">
+        <p>
+          Where can we contact you?
+        </p>
+      </div>
+      <form>
+        <input name="firstName" type="text" placeholder="First Name" required />
+        <input name="lastName" type="text" placeholder="Last Name" required />
+        <input name="email" type="email" placeholder="Email" />
+        <PhoneInput placeholder="Phone" />
+        <button
+          className="tertiary-button"
+          type="submit"
+          onClick={() => {
+            answer(info);
+          }}
+        >
+          Request Estimate
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default ContactInfo;
