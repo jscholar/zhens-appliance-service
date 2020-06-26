05e5401f17c42e31394eb902c97d29a8bede6765
Jin Cai <jinruncai@gmail.com>
2020-06-26 00:13:40 -0700

    Add brand field

--- src/client/components/Questionnaire/Brand.jsx
+++ src/client/components/Questionnaire/Brand.jsx
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const Brand = (props) => (
-  <div>
-    brand
-  </div>
-);
+import Input from './Input';
+import Selection from './Selection';
+
+const brands = [
+  { name: 'lg', svgLink: '/svg/brand-lg.svg', label: 'LG' },
+  { name: 'ge', svgLink: '/svg/brand-ge.svg', label: 'GE' },
+  { name: 'samsung', svgLink: '/svg/brand-samsung.svg', label: 'Samsung' },
+];
+
+const Brand = ({ answer }) => {
+  const [brand, setBrand] = useState('');
+
+  return (
+    <div className="brand">
+      <div className="prompt">
+        <p>What brand is your appliance?</p>
+      </div>
+      <div className="selection-choices">
+        {brands.map(({ name, svgLink, label }, index) => (
+          <Selection
+            key={name}
+            index={index}
+            handleSelect={() => {
+              setBrand(name);
+              answer(name);
+            }}
+            active={name === brand}
+            name={name}
+            svgLink={svgLink}
+            label={label}
+          />
+        ))}
+      </div>
+      <Input
+        prompt="Other"
+        fieldName="brand"
+        handleSubmit={answer}
+      />
+    </div>
+  );
+};
+
+Brand.propTypes = {
+  answer: PropTypes.func.isRequired,
+};
 
 export default Brand;
