e23e220baff73c5e4260b8486fa24ccfdd59696b
Jin Cai <jinruncai@gmail.com>
2020-06-27 00:01:28 -0700

    Remove unnecessary access token

--- src/server/auth/auth.js
+++ src/server/auth/auth.js
@@ -3,7 +3,6 @@ const {
   CLIENT_ID,
   CLIENT_SECRET,
   REFRESH_TOKEN,
-  ACCESS_TOKEN,
 } = process.env;
 
 const auth = {
@@ -12,7 +11,6 @@ const auth = {
   clientId: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
   refreshToken: REFRESH_TOKEN,
-  accessToekn: ACCESS_TOKEN,
 };
 
 module.exports = auth;
