ac580b8c6836ebb0ac5d6fe0630091f2905a20f9
Jin Cai <jinruncai@gmail.com>
2020-07-10 16:55:02 -0700

    Prevent references to browser env variables on server

--- src/client/api/loadMap.js
+++ src/client/api/loadMap.js
@@ -1,12 +1,23 @@
-import $script from 'scriptjs';
+/* eslint-disable global-require */
 
 const API_URL = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyC2I3FSzwLyC3Lf3jdf8yGcpimJi_g9IV8';
 
 let loadPromise;
+let $script = null;
 
 const loadMap = () => {
   if (!loadPromise) {
     loadPromise = new Promise((resolve, reject) => {
+      if (window === undefined) {
+        reject(new Error('cannot load outside of browser env'));
+        return;
+      }
+
+      /* Prevents references to browser env variables on server side */
+      if (!$script) {
+        $script = require('scriptjs');
+      }
+
       if (window.google && window.google.maps) {
         resolve(window.google.maps);
       }
