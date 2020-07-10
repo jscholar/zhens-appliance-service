7bb70ea2bd68b6a79d72a8bfa47f91da942c0111
Jin Cai <jinruncai@gmail.com>
2020-07-10 14:50:33 -0700

    Add basic about page content

--- src/client/pages/About.jsx
+++ src/client/pages/About.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
-const About = (props) => (
+const About = () => (
   <div className="about">
     About Zhen
+    <div className="frame">
+      <img src="/img/zhen.jpg" alt="Zhen and his work vehicle" />
+    </div>
+    <div>
+      Zhen is an appliance repair technician with over 20 years of professional experience.
+    </div>
   </div>
 );
 
