17013560f6bdab475decfbc6fd66bca5a21ad877
Jin Cai <jinruncai@gmail.com>
2020-07-20 16:01:19 -0700

    Update about page

--- src/client/pages/About.jsx
+++ src/client/pages/About.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import YelpIcon from '../components/YelpIcon';
 
 const About = () => (
   <div className="about">
-    About Zhen
+    <p className="title">
+      About Zhen
+    </p>
     <div className="frame">
       <img src="/img/zhen.jpg" alt="Zhen and his work vehicle" />
+      <p className="caption">
+        Zhen is an appliance repair technician with over 20 years of professional experience.
+      </p>
     </div>
-    <div>
-      Zhen is an appliance repair technician with over 20 years of professional experience.
+    <div className="yelp">
+      <p>Check us out on Yelp!</p>
+      <YelpIcon />
     </div>
   </div>
 );
